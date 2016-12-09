e3ea1d15d2041f3d1bd37ad91161676b25ac7a2c
SigridZhangSY <sigridzhang0603@163.com>
2016-12-09 11:30:11 +0800

    set routes contraints for the apia

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RailsApi::Application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+
+  namespace :api, defaults: { format: :json },
+  constraints: { subdomain: 'api'}, path: '/' do
 
   end
 
