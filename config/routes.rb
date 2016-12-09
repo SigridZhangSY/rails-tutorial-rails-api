ab72d3148a569fee35600c346afef5e3bbb5dd93
SigridZhangSY <sigridzhang0603@163.com>
2016-12-09 11:27:19 +0800

    set namespace and default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsApi::Application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
