6a8e409df1e4290c8145284407756608cb5e98c7
SigridZhangSY <sigridzhang0603@163.com>
2016-12-09 17:47:15 +0800

    shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe User do
+describe User, type: :model do
   before { @user = FactoryGirl.build(:user) }
 
   subject { @user}
@@ -9,5 +9,8 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 
-  it {should be_valid}
+  it { should validate_presence_of(:email) }
+  # it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
