5ab3353210b2a90eac806352d99faadd62f8da43
SigridZhangSY <sigridzhang0603@163.com>
2016-12-20 16:53:24 +0800

    product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209042608) do
+ActiveRecord::Schema.define(version: 20161220084123) do
+
+  create_table "products", force: true do |t|
+    t.string   "title",      default: ""
+    t.decimal  "price",      default: 0.0
+    t.boolean  "published",  default: false
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,8 +37,10 @@ ActiveRecord::Schema.define(version: 20161209042608) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
