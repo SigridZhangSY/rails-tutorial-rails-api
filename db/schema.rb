30d759b62b6f4134731840b9d1baf9fee730c8e2
SigridZhangSY <sigridzhang0603@163.com>
2016-12-21 09:52:06 +0800

    add order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220094400) do
+ActiveRecord::Schema.define(version: 20161221014838) do
+
+  create_table "orders", force: true do |t|
+    t.integer  "user_id"
+    t.decimal  "total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "products", force: true do |t|
     t.string   "title",      default: ""
