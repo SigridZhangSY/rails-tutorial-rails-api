30f9c17e2598662751ddd2fb9cc9fcbf0cf87dde
SigridZhangSY <sigridzhang0603@163.com>
2016-12-21 10:47:17 +0800

    create order: 201, url

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,4 +10,18 @@ class OrdersController < ApplicationController
     user = User.find(params[:user_id])
     respond_with user.orders.find(params[:id])
   end
+
+  def create
+    user = User.find(params[:user_id])
+    order = user.orders.build(order_params)
+    order.save
+    order_url = Hash[:user_id => order[:user_id], :id => order[:id]]
+    render json: order, status: 201, location: user_order_url(order_url)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:total, :user_id, :product_ids => [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ RailsApi::Application.routes.draw do
   resources :users, :only => [:show, :create, :update, :destroy] do
 
     resources :products, :only => [:show, :index, :create, :update, :destroy]
-    resources :orders, :only => [:index, :show]
+    resources :orders, :only => [:index, :show, :create]
 
   end
   # end
